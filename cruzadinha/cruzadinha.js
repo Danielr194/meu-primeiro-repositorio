1ffee988d75254ea1356c6097ee08c9d0190836d
Danielr194 <125319192+Danielr194@users.noreply.github.com>
2023-05-28 22:58:19 -0300

    Add files via upload

---
+++ cruzadinha/cruzadinha.js
@@ -0,0 +1,208 @@
+function validacao(input) {
+    if (flash_letra1.value == 'F') {
+        if (flash_letra2.value == 'L') {
+            if (databus_letra4_flash_letra3.value == 'A') {
+                if (flash_letra4.value == 'S') {
+                    if (flash_letra5.value == 'H') {
+                        alert('flash')
+                    }
+                }
+            }
+        }
+    }
+
+
+    if (dma_letra1.value == 'D') {
+        if (dma_letra2.value == 'M') {
+            if (memoriaDeMassa_letra7_dma_letra3.value == 'A') {
+                alert('dma')
+            }
+        }
+    }
+
+
+
+    if (memoriaDeMassa_letra1.value == 'M') {
+        if (memoriaDeMassa_letra2_eprom_letra1.value == 'E') {
+            if (memoriaDeMassa_letra3_rom_letra3.value == 'M') {
+                if (memoriaDeMassa_letra4.value == 'O') {
+                    if (memoriaDeMassa_letra5.value == 'R') {
+                        if (memoriaDeMassa_letra6_i5_letra1.value == 'I') {
+                            if (memoriaDeMassa_letra7_dma_letra3.value == 'A') {
+                                if (memoriaDeMassa_letra8.value == 'D') {
+                                    if (memoriaDeMassa_letra9_addressbus_letra5.value == 'E') {
+                                        if (memoriaDeMassa_letra10.value == 'M') {
+                                            if (memoriaDeMassa_letra11.value == 'A') {
+                                                if (memoriaDeMassa_letra12.value == 'S') {
+                                                    if (memoriaDeMassa_letra13.value == 'S') {
+                                                        if (memoriaDeMassa_letra14_databus_letra2.value == 'A') {
+                                                            alert('memoria de massa')
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    if (memoriaDeMassa_letra6_i5_letra1.value == 'I') {
+        if (i5_letra2.value == '5') {
+            alert('i5')
+        }
+    }
+
+
+    if (memoriaDeMassa_letra2_eprom_letra1.value == 'E') {
+        if (eprom_letra2.value == 'P') {
+            if (eprom_letra3.value == 'R') {
+                if (eprom_letra4.value == 'O') {
+                    if (eprom_letra5.value == 'M') {
+                        alert('eprom')
+                    }
+                }
+            }
+        }
+    }
+
+  
+
+    if (ram_letra1_rom_letra1.value == 'R') {
+        if (ram_letra2.value == 'A') {
+            if (ram_letra3.value == 'M') {
+                alert('ram')
+            }
+        }
+    }
+
+    if (addressbus_letra9_ula_letra1.value == 'U') {
+        if (ula_letra2.value == 'L') {
+            if (ula_letra3.value == 'A') {
+                alert('ula')
+            }
+        }
+    }
+
+
+  
+
+
+    if (ram_letra1_rom_letra1.value == 'R') {
+        if (rom_letra2.value == 'O') {
+            if (memoriaDeMassa_letra3_rom_letra3.value == 'M') {
+                alert('rom')
+
+            }
+        }
+    }
+
+    if (cpu_letra1.value == 'C') {
+        if (cpu_letra2.value == 'P') {
+            if (databus_letra6_cpu_letra3.value == 'U') {
+                alert('cpu')
+            }
+        }
+    }
+
+
+    if (registradores_letra1.value == 'R') {
+        if (registradores_letra2.value == 'E') {
+            if (registradores_letra3.value == 'G') {
+                if (registradores_letra4_i7_letra1.value == 'I') {
+                    if (registradores_letra5.value == 'S') {
+                        if (registradores_letra6.value == 'T') {
+                            if (registradores_letra7.value == 'R') {
+                                if (registradores_letra8_Addressbus_letra1.value == 'A') {
+                                    if (registradores_letra9.value == 'D') {
+                                        if (registradores_letra10.value == 'O') {
+                                            if (registradores_letra11.value == 'R') {
+                                                if (registradores_letra12.value == 'E') {
+                                                    if (registradores_letra13.value == 'S') {
+                                                        alert('registradores')
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    if (registradores_letra4_i7_letra1.value == 'I') {
+        if (i7_letra2.value == '7') {
+            alert('i7')
+        }
+
+    }
+
+    if (registradores_letra8_Addressbus_letra1.value == 'A') {
+        if (addressbus_letra2.value == 'D') {
+            if (addressbus_letra3.value == 'D') {
+                if (addressbus_letra4.value == 'R') {
+                    if (memoriaDeMassa_letra9_addressbus_letra5.value == 'E') {
+                        if (addressbus_letra6.value == 'S') {
+                            if (addressbus_letra7_cs_letra2.value == 'S') {
+                                if (addressbus_letra8.value == 'B') {
+                                    if (addressbus_letra9_ula_letra1.value == 'U') {
+                                        if (addressbus_letra10.value == 'S') {
+                                            alert('addressbus')
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+
+if (cs_letra1.value == 'C') {
+    if (addressbus_letra7_cs_letra2.value == 'S') {
+        alert('cs')
+    }
+}
+
+
+
+
+if (databus_letra1.value == 'D') {
+    if (memoriaDeMassa_letra14_databus_letra2.value == 'A') {
+        if (databus_letra3.value == 'T') {
+            if (databus_letra4_flash_letra3.value == 'A') {
+                if (databus_letra5.value == 'B') {
+                    if (databus_letra6_cpu_letra3.value == 'U') {
+                        if (databus_letra7.value == 'S') {
+                            alert('databus')
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+ }
